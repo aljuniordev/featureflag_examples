6d24d37eea3868e3b6c14a3324b31ab0e705d09b
alcemir.r.junior <alcemir.r.junior@accenture.com>
2024-10-24 09:16:01 -0300

    update bean objectmapper

--- src/main/java/com/example/featureflag/domain_rules_example/common/JacksonMapper.java
+++ src/main/java/com/example/featureflag/domain_rules_example/common/JacksonMapper.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileReader;
@@ -35,6 +36,11 @@ public class JacksonMapper {
         SimpleModule simpleModule = new SimpleModule();
         registerModule(simpleModule);
     }};
+
+    @Bean
+    public ObjectMapper updateBean() {
+        return objectMapper;
+    }
 
     public static String toJson(Object obj) {
         try {
